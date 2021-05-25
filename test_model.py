4912ff461a5897b3764d53ff194547bff2beae6d
Alex González <alexantoniogonzalez2@gmail.com>
2021-05-25 12:12:19 +1000

    read image

--- test_model.py
+++ test_model.py
@@ -1,28 +1,20 @@
 import requests
-url = 'http://1396f0685351.ngrok.io/predict'
-#f = open('cloth.jpg', 'rb')
-#files = {"file": (f.name, f, "multipart/form-data")}
-file_path = 'cloth.jpg'
-
-#files = {'my_file': open('cloth.jpg', 'rb')}
-#files = {'filename':file_path,'content_type':"image/jpeg",'file':open(file_path,'rb')}
-#files = {'file':(file_path, open(file_path, 'rb'), "multipart/form-data")}
 import json
 import base64
+
+url = 'http://2d20c4ef39bd.ngrok.io/predict'
+file_path = 'data/test/cloth/000003_1.jpg'
 
 data = {}
 with open(file_path , mode='rb') as file:
     img = file.read()
 data['img'] = base64.encodebytes(img).decode('utf-8')
 
-
-
-
-#json_file = open(file_path,'rb').read()
-
 res = requests.post(url, json=json.dumps(data))
 
 print(res)
-
 if res.ok:
-    print(res.json())
+	with open("image.jpg", "wb") as fp:
+	    content = base64.b64decode(res.json()['img'])
+	    fp.write(content)
+	    
